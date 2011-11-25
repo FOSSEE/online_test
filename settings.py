511c79e25a07fdab2810909d52263763745979e1
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2011-11-26 01:09:25 +0530

    Updating settings to add URL_ROOT for admin media.

--- settings.py
+++ settings.py
@@ -89,7 +89,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = URL_ROOT + '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
