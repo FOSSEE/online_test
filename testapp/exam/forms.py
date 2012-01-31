e7f3f0436555251577f61c93f2e7c1a99c9028a1
jayparikh111 <jayparikh111@gmail.com>
2012-01-31 19:45:00 +0530

    Updated forms.py to handle and exception when username or password fields are kept blank

--- testapp/exam/forms.py
+++ testapp/exam/forms.py
@@ -85,8 +85,11 @@ class UserLoginForm(forms.Form):
 
     def clean(self):
         super(UserLoginForm, self).clean()
-        u_name, pwd = self.cleaned_data["username"], self.cleaned_data["password"]
-        user = authenticate(username = u_name, password = pwd)
+	try:
+	        u_name, pwd = self.cleaned_data["username"], self.cleaned_data["password"]
+	        user = authenticate(username = u_name, password = pwd)
+	except Exception:
+		raise forms.ValidationError("Username and/or Password is not entered")
 
         if not user:
             raise forms.ValidationError("Invalid username/password")
