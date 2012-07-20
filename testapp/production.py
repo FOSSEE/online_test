88963ad2a4991260ea106d18bea55ad019446b4c
Parth Buch <parth.buch.115@gmail.com>
2012-07-20 16:13:10 +0530

    Removed the database settings from production.py, now relies on local.py

--- testapp/production.py
+++ testapp/production.py
@@ -2,13 +2,5 @@ from testapp.settings import *
 
 DEBUG=False
 TEMPLATE_DEBUG=DEBUG
-
-
-DATABASES["default"]["ENGINE"] = 'django.db.backends.mysql'
-DATABASES["default"]["NAME"] = 'online_test'
-DATABASES["default"]["USER"] = 'online_test_user'
-
-from testapp.local import DATABASE_PASSWORD
-# Imports DATABASE_PASSWORD from testapp/local.py that is not part of git repo
-DATABASES["default"]["PASSWORD"] = DATABASE_PASSWORD
+from testapp.local import *
 
