e7725d052492ac6d93e3dfaa74b18985cc89e388
hardythe1 <hardy_the1@yahoo.com>
2014-06-19 17:53:42 +0530

    migrating to django 1.6 - removing defaults as it is no longer needed in

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, url, include
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
