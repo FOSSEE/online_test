e89f0b0e3a74195b3bce0f68c337ead9dc18a11e
prathamesh <pratham_920@yahoo.com>
2014-07-11 13:42:35 +0530

    Exam app using django 1.6.5
    Removed version specification to install latest
    version of django.
    Added settings.py to the exam app.
    Removed default from import statement in urls.py
    since it is deprecated.

---
+++ testapp/exam/settings.py
@@ -0,0 +1,20 @@
+"""
+settings for exam app.
+"""
+# The ports the code server should run on.  This will run one separate
+# server for each port listed in the following list.
+SERVER_PORTS = [8001]  # range(8001, 8026)
+
+# The server pool port.  This is the server which returns available server
+# ports so as to minimize load.  This is some random number where no other
+# service is running.  It should be > 1024 and less < 65535 though.
+SERVER_POOL_PORT = 53579
+
+# Timeout for the code to run in seconds.  This is an integer!
+SERVER_TIMEOUT = 2
+
+# The root of the URL, for example you might be in the situation where you
+# are not hosted as host.org/exam/  but as host.org/foo/exam/ for whatever
+# reason set this to the root you have to serve at.  In the above example
+# host.org/foo/exam set URL_ROOT='/foo'
+URL_ROOT = ''
