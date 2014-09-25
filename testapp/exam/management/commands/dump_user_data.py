a51ae0eddcbc689e01d5f7843b95916561830c55
prathamesh <pratham_920@yahoo.com>
2014-09-25 16:28:43 +0530

    modified import path in exam commands

--- testapp/exam/management/commands/dump_user_data.py
+++ testapp/exam/management/commands/dump_user_data.py
@@ -5,8 +5,8 @@ from django.core.management.base import BaseCommand
 from django.template import Template, Context
 
 # Local imports.
-from exam.views import get_user_data
-from exam.models import User
+from testapp.exam.views import get_user_data
+from testapp.exam.models import User
 
 data_template = Template('''\
 ===============================================================================
