c69c414120f85ed61b62451bae3b806d93c52361
maheshgudi <mahesh.p.gudi@gmail.com>
2016-05-13 18:19:34 +0530

    added js function to alert user for prerequisites

--- yaksh/static/yaksh/js/add_quiz.js
+++ yaksh/static/yaksh/js/add_quiz.js
@@ -9,3 +9,12 @@ function test()
         document.getElementById("submit").innerHTML = "Save";
     }
 }
+
+function usermode(location)
+{
+  var select = document.getElementById("id_prerequisite");
+  var select_text = select.options[select.selectedIndex].text;
+  window.alert(select_text + " is a prerequisite for this course.\n \
+  				You are still allowed to attempt this quiz.")
+  window.location.replace(location);
+}
