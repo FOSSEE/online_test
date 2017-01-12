e87cb6c64ac30997a8a66846b0af7fdc87d47eac
maheshgudi <mahesh.p.gudi@gmail.com>
2017-01-12 16:20:40 +0530

    yadded search option in answerpaper admin page, gitignore doc _builds
    1. added search option in answerpaper admin page, helpful to find specific answerpapers
    2. modified gitinore file, ignores _builds in docs now

--- yaksh/admin.py
+++ yaksh/admin.py
@@ -2,6 +2,13 @@ from yaksh.models import Question, Quiz, QuestionPaper
 from yaksh.models import TestCase, StandardTestCase, StdIOBasedTestCase, Course, AnswerPaper
 from django.contrib import admin
 
+
+
+class AnswerPaperAdmin(admin.ModelAdmin):
+	search_fields = ['user__first_name', 'user__last_name','user__username',
+					 "question_paper__quiz__description" ]
+
+
 admin.site.register(Question)
 admin.site.register(TestCase)
 admin.site.register(StandardTestCase)
@@ -9,4 +16,4 @@ admin.site.register(StdIOBasedTestCase)
 admin.site.register(Course)
 admin.site.register(Quiz)
 admin.site.register(QuestionPaper)
-admin.site.register(AnswerPaper)
+admin.site.register(AnswerPaper, AnswerPaperAdmin)
