b277f9191bf8720533901e58db36851675b8f430
maheshgudi <mahesh.p.gudi@gmail.com>
2017-02-03 01:28:50 +0530

    added skip question feature. closes #191
    The can_skip_questions attribute is to be set True for students to be able to skip questions.
    If it is set as False, students wont be able to skip to the next question.

--- yaksh/models.py
+++ yaksh/models.py
@@ -580,6 +580,10 @@ class Quiz(models.Model):
     view_answerpaper = models.BooleanField('Allow student to view their answer\
                                             paper', default=False)
 
+    can_skip_questions = models.BooleanField("Allow students to skip questions",
+                                             default=True
+                                             )
+
     objects = QuizManager()
 
     class Meta:
