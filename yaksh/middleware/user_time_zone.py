09429a3a01a591e1a87df51e8584bb4358508b67
adityacp <aditya94palaparthy@gmail.com>
2017-04-07 15:58:20 +0530

    Change Default timezone from UTC to Asia/Kolkata

--- yaksh/middleware/user_time_zone.py
+++ yaksh/middleware/user_time_zone.py
@@ -5,10 +5,10 @@ from django.utils import timezone
 
 class TimezoneMiddleware(object):
     """ Middleware to get user's timezone and activate timezone 
-        if user timezone is not available default value 'UTC' is activated """
+        if user timezone is not available default value 'Asia/Kolkata' is activated """
     def process_request(self, request):
         user = request.user
-        user_tz = 'UTC'
+        user_tz = 'Asia/Kolkata'
         if hasattr(user, 'profile'):
             if user.profile.timezone:
                 user_tz = user.profile.timezone
