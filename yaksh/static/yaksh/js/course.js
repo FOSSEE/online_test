2d62359adf671d5a75201a2314825039adc7291a
adityacp <aditya94palaparthy@gmail.com>
2017-05-15 15:03:21 +0530

    Change course.js and course_detail.html - Allow moderator to send emails to students enrollded in a course - Add validations to check email data in course.js

--- yaksh/static/yaksh/js/course.js
+++ yaksh/static/yaksh/js/course.js
@@ -35,4 +35,49 @@ $(".reject").change( function(){
                 });
         }
     });
+
+$(function() {
+    $('textarea#email_body').froalaEditor({
+        heightMin: 100,
+        heightMax: 200
+    })
+  });
+
+var status;
+var btn_name;
+
+$("#send_mail").click(function(){
+    btn_name = "send_mail";
+    var subject = $("#subject").val();
+    var body = $('#email_body').val();
+    if (subject == '' || body == ''){
+        status = false;
+        $("#error_msg").html("Please enter email details");
+        $("#dialog").dialog();
+    }
+    else{
+        status = true;
+    }
+});
+
+$('#reject-form').submit(function(eventObj) {
+    if (btn_name == 'send_mail'){
+        if(status == false){
+            return false;
+        }
+    }
+    var selected = [];
+    $('#reject input:checked').each(function() {
+        selected.push($(this).attr('value'));
+    });
+    if(selected.length > 0){
+        return true;
+    }
+    else{
+        $("#error_msg").html("Please select atleast one user");
+        $( "#dialog" ).dialog();
+        return false;
+    }
+});
+
 });
