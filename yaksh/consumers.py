69594444d8f3a6cf44421be666017150f235bd17
adityacp <aditya94palaparthy@gmail.com>
2017-09-21 16:44:15 +0530

    Change channel session name in consumers,py

--- yaksh/consumers.py
+++ yaksh/consumers.py
@@ -12,9 +12,8 @@ from yaksh.models import Room, User
 @channel_session_user_from_http
 def ws_connect(message):
     try:
-        path = message['path'].split('/')[1:]
         user = message.user
-        prefix, label, course_id = path
+        prefix, label, course_id = message['path'].split('/')[1:]
         if prefix != 'chat':
             message.reply_channel.send({'close': True})
             return
@@ -33,8 +32,8 @@ def ws_connect(message):
     except Room.DoesNotExist:
         message.reply_channel.send({'close': True})
         return
-
-    Group('chat-'+label).add(message.reply_channel)
+    Group("chat_room_"+str(room.course.id)).add(
+        message.reply_channel)
     message.channel_session['course'] = room.course_id
     message.channel_session['room'] = room.label
     message.reply_channel.send({'accept': True})
@@ -46,7 +45,6 @@ def ws_receive(message):
     try:
         course = message.channel_session['course']
         room = Room.objects.get(course_id=course)
-        label = message.channel_session['room']
     except (KeyError, Room.DoesNotExist):
         message.reply_channel.send({'close': True})
         return
@@ -57,7 +55,6 @@ def ws_receive(message):
     except ValueError:
         message.reply_channel.send({'close': True})
         return
-
     if set(data.keys()) != set(('sender_id', 'message')):
         message.reply_channel.send({'close': True})
         return
@@ -66,25 +63,26 @@ def ws_receive(message):
         context = {}
         chat_dict = {}
         message = room.messages.create(**data)
-        sender_tz = User.objects.get(id=data['sender_id']).profile.timezone
-        msg_date = message.timestamp.astimezone(pytz.timezone(sender_tz))
         chat_dict = {
             "sender": message.sender.id,
             "sender_name": message.sender.get_full_name().title(),
-            "timestamp": datetime.strftime(msg_date, "%Y-%m-%d %H:%M:%S"),
+            "timestamp": datetime.strftime(message.timestamp,
+                                           "%Y-%m-%dT%H:%M:%S"),
             "message": message.message
         }
         context['messages'] = chat_dict
         context['success'] = True
-        Group('chat-'+label).send({'text': json.dumps(context)})
+        Group("chat_room_"+str(room.course.id)).send(
+            {'text': json.dumps(context)})
 
 
 @channel_session
 def ws_disconnect(message):
     try:
-        label = message.channel_session['room']
-        Room.objects.get(label=label)
-        Group('chat-'+label).discard(message.reply_channel)
+        course_id = message.channel_session['course']
+        room = Room.objects.get(course_id=course_id)
+        Group("chat_room_"+str(room.course.id)).discard(
+            message.reply_channel)
     except (KeyError, Room.DoesNotExist):
         pass
     message.reply_channel.send({'close': True})
