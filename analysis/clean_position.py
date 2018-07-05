f79e5f8a442f96d52816d3e47ac7949336b548f0
skt7 <shivamthakkar5@gmail.com>
2018-07-05 23:16:34 +0530

    added scripts to clean Profile-position and Course-group fields

--- analysis/clean_position.py
+++ analysis/clean_position.py
@@ -8,6 +8,7 @@ Created on Fri Jun 15 02:07:57 2018
 
 from yaksh.models import *
 import numpy as np
+from yaksh.views import is_moderator
 
 default = ["Faculty", "School Student", "Graduate Student", 
                 "Postgraduate Student", "Industry Professional", 
@@ -19,7 +20,14 @@ profiles = Profile.objects.all()
 for profile in profiles:
     val = profile.position.lower()
 
-    if val == '' or 'b.tech' in val or val=='student' or 'year' in val or 'fresh' in val \
+    # if 'teacher' in val or 'prof' in val or 'faculty' in val or \
+    # 'instuct' in val or 'assist' in val or 'lect' in val:
+        #print(profile.user, val, is_moderator(profile.user))
+        
+    if is_moderator(profile.user):
+        new_val = 'Faculty'
+
+    elif val == '' or 'b.tech' in val or val=='student' or 'year' in val or 'fresh' in val \
         or 'sem' in val or val.isdigit() or 'btech' in val:
         new_val = 'Graduate Student'    
 
@@ -28,11 +36,7 @@ for profile in profiles:
 
     elif 'engineer' in val or 'professional' in val or 'industry' in val or 'manage' in val \
             or 'dev' in val or 'program' in val or 'soft' in val:
-        new_val = 'Industry Professional'
-
-    elif 'teacher' in val or 'prof' in val or 'faculty' in val or \
-        'instuct' in val or 'assist' in val or 'lect' in val:
-        new_val = 'Faculty'
+        new_val = 'Industry Professional'        
 
     elif 'research' in val:
         new_val = 'Research Scholar'
@@ -45,8 +49,8 @@ for profile in profiles:
     position.append([val, new_val])
     #print(val, new_val)
 
-    #profile.position = new_val
-    #profile.save()
+    profile.position = new_val
+    profile.save()
 
 position = np.array(position)
 print(np.unique(position[:, 1:], return_counts=True))
