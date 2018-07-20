477ff979058b6f64dd6af05122914a900e9e6810
adityacp <aditya94palaparthy@gmail.com>
2018-07-20 11:57:54 +0530

    Make course_id compulsory argument in download_assignment_file view function and format error message

--- yaksh/views.py
+++ yaksh/views.py
@@ -2098,14 +2098,14 @@ def update_email(request):
 
 @login_required
 @email_verified
-def download_assignment_file(request, quiz_id, course_id=None,
+def download_assignment_file(request, quiz_id, course_id,
                              question_id=None, user_id=None):
     user = request.user
     course = Course.objects.get(id=course_id)
     if (not course.is_creator(user) and not course.is_teacher(user) and
             not course.is_student(user)):
-        raise Http404("You are not allowed to download {}".format(
-            course.name.replace(" ", "_"))
+        raise Http404("You are not allowed to download files for {0}".format(
+            course.name)
         )
     qp = QuestionPaper.objects.get(quiz_id=quiz_id)
     assignment_files, file_name = AssignmentUpload.objects.get_assignments(
