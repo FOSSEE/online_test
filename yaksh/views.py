ed532c50d3f5c61ba3ce39d07c8c0c21e6c9c171
CruiseDevice <meakashchavan@gmail.com>
2019-02-13 15:50:00 +0530

    Add Preview QuestionPaper button in Quiz view page

--- yaksh/views.py
+++ yaksh/views.py
@@ -2822,8 +2822,6 @@ def _update_course_percent(course, user):
 @email_verified
 def preview_questionpaper(request, questionpaper_id):
     user = request.user
-    if not is_moderator(user):
-        raise Http404('You are not allowed to view this page!')
     paper = QuestionPaper.objects.get(id=questionpaper_id)
     if not paper.quiz.creator == user:
         raise Http404('This questionpaper does not belong to you')
