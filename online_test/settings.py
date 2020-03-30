c3a18de3b8f120b16dea005d2b0ce375804f24ec
CruiseDevice <meakashchavan@gmail.com>
2020-03-30 20:23:40 +0530

    Add corsheaders to INSTALLED_APPS

--- online_test/settings.py
+++ online_test/settings.py
@@ -47,10 +47,13 @@ INSTALLED_APPS = (
     'grades',
     'rest_framework',
     'api',
+    'corsheaders',
+    'rest_framework.authtoken'
 )
 
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
