1bf1f2e45e69689acd2a23b2940ef200b9a98467
WISE-ELITE-2K18 <71124262+WISE-ELITE-2K18@users.noreply.github.com>
2020-09-14 19:29:47 +0530

    Update send_emails.py

--- yaksh/send_emails.py
+++ yaksh/send_emails.py
@@ -91,6 +91,9 @@ def send_bulk_mail(subject, email_body, recipients, attachments):
 
 
 def mail_certificate(send_to):
-    email = EmailMessage('Certificate', 'Congrats!! You successfully completed the course', settings.EMAIL_HOST_USER, [send_to,], )
+    email = EmailMessage('Certificate',
+                         'Congrats!! You successfully completed the course',
+                         settings.EMAIL_HOST_USER,
+                         [send_to, ], )
     email.attach_file('certificate.pdf')
     email.send()
