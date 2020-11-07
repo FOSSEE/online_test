6b5b21fc26879c1724bf02952584196f6c302b91
adityacp <aditya94palaparthy@gmail.com>
2020-11-07 11:58:22 +0530

    Add tests for lesson tracking

--- stats/views.py
+++ stats/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.http import Http404
 
 # Local Imports
 from stats.models import TrackLesson
