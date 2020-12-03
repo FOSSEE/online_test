e461b7c71d3d33f7e1cde4415ae35733078f4070
adityacp <aditya94palaparthy@gmail.com>
2020-12-03 14:32:15 +0530

    Show graphs for the lesson video statistics

--- yaksh/templatetags/custom_filters.py
+++ yaksh/templatetags/custom_filters.py
@@ -209,5 +209,5 @@ def get_tc_percent(tc_id, data):
 def get_lesson_views(course_id, lesson_id):
     course = Course.objects.get(id=course_id)
     return TrackLesson.objects.filter(
-        course_id=course_id, lesson_id=lesson_id
+        course_id=course_id, lesson_id=lesson_id, watched=True
     ).count(), course.students.count()
